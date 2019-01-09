36392887e101566b57cb353a48685a328f047bb2
zakklepac <41202649+zakklepac@users.noreply.github.com>
2019-01-09 15:33:04 -0600

    update

--- linkedlist.js
+++ linkedlist.js
@@ -12,17 +12,34 @@ class LinkedList {
     add(data) {
 
         if (this.length === 0) {
-            this.head = this.tail = new Node(data)
+            this.head = this.tail = new Node(data);
         } else {
-            let newNode = new Node(data, this.tail)
-            this.tail.next = newNode
-            this.tail = newNode
+            let newNode = new Node(data, this.tail);
+            this.tail.next = newNode;
+            this.tail = newNode;
         }
-        this.length++
-        return this
+        this.length++;
+        return this;
     }
 
-    //indexOf()
+    get(i, node) {
+        if (Object.is(node, undefined)) {
+            node = this.head;
+        }
+        if (i === 0) {
+            return node;
+        } else {
+            return this.get(--i, node.next)
+        }
+    }
+
+    indexOf(index) {
+        let node = this.get(index);
+        return node.data;
+    }
+
+
+
     //insertAt()
     //removeAt()
 }
