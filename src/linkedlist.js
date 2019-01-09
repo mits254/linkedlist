f7b685cc245297ecc54a3840f4d83a014f5d52e8
zakklepac <41202649+zakklepac@users.noreply.github.com>
2019-01-09 14:05:53 -0600

    burned down tests and started over

---
+++ src/linkedlist.js
@@ -0,0 +1,16 @@
+const Node = require('./node');
+
+class LinkedList {
+    constructor() {
+
+        this.length = 0;
+        this._head = null;
+        this._tail = null;
+
+    }
+
+    //add()
+    //indexOf()
+    //insertAt()
+    //removeAt()
+}
--- test/linkedlist.test.js
+++ test/linkedlist.test.js
@@ -1,77 +1,84 @@
-const expect = require('chai').expect
-const linkedList = require('../linkedlist')
+const Node = require('../src/node')
+const LinkedList = require('../src/linked-list')
 
 
+    describe('LinkedList', () => {
+        describe('#constructor', () => {
+            const list = new LinkedList()
 
-
-describe("linked list", () => {
-    describe("creating a linked list", () => {
-        it("should create a linked list", () =>{
-            const list = new linkedList ()
-            
-            list.add('hey')
-            list.add('sup')
-
-            expect(list.length).to.equal(2)
-        })
+            it('assign 0 to this.length', () => {
+                expect(list.length).to.equal(0);
+            })
+        });
     })
-})
 
-describe("indexOf", () => {
-    describe("it returns the index of a given element", () => {
-        it("should return a number", () =>{
-            const list = new linkedList ()
+    describe('add', () => {
+        it('should assign any nodes to this._head and this._tail if list is empty', () => {
+            const data = 101;
 
-            list.add(123)
-            list.add(456)
-            list.add(789)
+            const list = new LinkedList();
 
-            expect(list.indexOf(0)).to.equal(123);
-            expect(list.indexOf(1)).to.equal(456);
-            expect(list.indexOf(2)).to.equal(789);
-        })
-    })
-})
+            list.add(data);
 
-describe("isEmpty", () => {
-    describe("if the list is empty", () => {
-        it("should return true", () =>{
-            const list = new linkedList ()
+            expect(list._tail).to.be.an.instanceof(Node)
+            expect(list._head).to.be.an.instanceof(Node)
+        });
 
-            expect(list).to.equal(true)
-        })
-    })
-})
-
-describe("add", () => {
-    describe("adds an element at the end of list", () => {
-        it("create a new index at the end", () =>{
-            const list = new linkedList ()
+        it('should add new data to the end of list', () => {
+            const list = new LinkedList();
 
             list.add(666);
             list.add(420);
 
             expect(list.length).to.equal(2);
-            expect(list.indexOf(1)).to.equal(420);
+            expect(list.tail()).to.equal(666);
+            expect(list.head()).to.equal(420);
+        });
 
-        })
     })
-})
 
-describe("removeFrom", () => {
-    describe("removes and returns an element from the list", () => {
-        it("return a specific index", () =>{
-            const list = new linkedList ()
+    describe('indexOf', () => {
+        it('should return Node.data by index', () => {
+            const list = new LinkedList();
 
-            list.add(1);
-            list.add(2);
-            list.add(3);
-            list.add(4);
-            list.add(5);
+            list.add(123);
+            list.add(456);
+            list.add(789);
 
-            list.removeFrom(2)
-            
+            expect(list.at(0)).to.equal(123);
+            expect(list.at(1)).to.equal(456);
+            expect(list.at(2)).to.equal(789);
+
+        });
+    })
+
+    describe('removeAt', () => {
+        it('should delete element by index', () => {
+            const list = new LinkedList();
+
+            list.append(1);
+            list.append(2);
+            list.append(3);
+            list.append(4);
+            list.append(5);
+
+            list.deleteAt(2);
+
             expect(list.at(2)).to.equal(4);
-        })
-    })
-})
+        });
+    });
+
+    describe('insertAt', () => {
+        it('should insert data by index', () => {
+            const list = new LinkedList();
+            const data = 9001;
+            const position = 1;
+
+            list.append(32);
+            list.append(47);
+
+            list.insertAt(position, data);
+
+            expect(list.at(position)).to.equal(data);
+        });
+    });
