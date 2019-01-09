348a530266797472783d239475a34997f7b40fce
zakklepac <41202649+zakklepac@users.noreply.github.com>
2019-01-09 14:27:47 -0600

    add halfway works

--- linkedlist.js
+++ linkedlist.js
@@ -1,14 +1,30 @@
-class LinkedList { 
-    constructor() 
-    { 
-        this.head = null; 
-        this.size = 0; 
-    } 
-  
-    // add(element)  
-    // removeFrom(location) 
-  
-    // isEmpty 
+const Node = require('./node');
 
-} 
+class LinkedList {
+    constructor() {
 
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+
+    }
+
+    add(data) {
+
+        if (this.length === 0) {
+            this.head = this.tail = new Node(data)
+        } else {
+            let newNode = new Node(data, this.tail)
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
+
+    //indexOf()
+    //insertAt()
+    //removeAt()
+}
+
+module.exports = LinkedList
